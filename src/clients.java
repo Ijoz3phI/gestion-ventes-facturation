6f796171b145b98c2091960d65841dc1a468b42d
varuxba <bellagridanass@gmail.com>
2025-12-10 19:19:35 +0100

    classe clients

---
+++ src/clients.java
@@ -0,0 +1,13 @@
+public class clients {
+    private String nom;
+    private String prenom;
+    private int num ;
+    
+
+    public clients(String nom, int num, String prenom) {
+        this.nom = nom;
+        this.num = num;
+        this.prenom = prenom;
+    }
+
+}
