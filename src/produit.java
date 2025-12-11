4aa71a4473e740074fbe68764b2bcf86ab1a9edf
Anass <anassbellagrid@gmail.com>
2025-12-11 22:56:11 +0100

    suite

--- src/produit.java
+++ src/produit.java
@@ -1,9 +1,16 @@
 public class produit {
 
-     private int idProduit;
+     private int idproduit;
     private String nom;
     private double prixHT;
     private double tva;
     private int stock;
 
-}
+public produit (int idproduit, String nom, double prixHT, double tva, int stock) {
+    this.idproduit = idproduit;
+    this.nom = nom;
+    this.prixHT = prixHT;
+    this.tva = tva;
+    this.stock = stock;  }
+
+} 
