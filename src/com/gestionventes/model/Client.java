e688c7186301f9afe405ba116d32baf4456ff37a
ljoz3phl <youssefbsibiss2005@gmail.com>
2025-12-13 20:04:56 +0100

    ajout de la class client fr this time ;(

--- src/com/gestionventes/model/Client.java
+++ src/com/gestionventes/model/Client.java
@@ -2,4 +2,78 @@ package com.gestionventes.model;
 
 public class Client {
     private int id ;
+    private String nom ;
+    private String prenom ;
+    private String telephone ;
+    private String adresse ;
+    private String email ;
+
+    public Client(int id, String nom, String prenom, String telephone, String adresse, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+        this.adresse = adresse;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
