db84d51161686f69a3da3290dd1f090652a5f5dc
Anass <anassbellagrid@gmail.com>
2025-12-13 21:49:58 +0100

    c

--- src/com/gestionventes/model/Produit.java
+++ src/com/gestionventes/model/Produit.java
@@ -2,4 +2,15 @@ package com.gestionventes.model;
 
 public class Produit {
 
+    private int id;
+    private String libelle;
+    private double prix;
+    private int stock;
+
+    public Produit(int id, String libelle, double prix, int stock) {
+        this.id = id;
+        this.libelle = libelle;
+        this.prix = prix;
+        this.stock = stock;
+    }
 }
