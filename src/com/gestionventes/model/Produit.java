97aa2750ae886d0dc5d7ee6d670cc6a6bc9e45d3
Anass <anassbellagrid@gmail.com>
2025-12-13 22:11:34 +0100

    v

--- src/com/gestionventes/model/Produit.java
+++ src/com/gestionventes/model/Produit.java
@@ -13,4 +13,13 @@ public class Produit {
         this.prix = prix;
         this.stock = stock;
     }
+
+    public int getId() { return id; }
+    public String getLibelle() { return libelle; }
+    public void setLibelle(String libelle) { this.libelle = libelle; }
+    public double getPrix() { return prix; }
+    public void setPrix(double prix) { this.prix = prix; }
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
+
 }
