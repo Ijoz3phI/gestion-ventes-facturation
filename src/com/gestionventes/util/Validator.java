fc124e508ae4d99ce4a42a9a831b02c9d29ff1b6
Anass <anassbellagrid@gmail.com>
2025-12-15 15:48:30 +0100

    validator

--- src/com/gestionventes/util/Validator.java
+++ src/com/gestionventes/util/Validator.java
@@ -1,5 +1,16 @@
 package com.gestionventes.util;
 
 public class Validator {
+    
+ public static void verifierPrix(double prix) {
+        if (prix < 0) {
+            throw new IllegalArgumentException("Le prix ne peut pas être négatif");
+        }
+    }
 
+    public static void verifierStock(int stock) {
+        if (stock < 0) {
+            throw new IllegalArgumentException("Le stock ne peut pas être négatif");
+        }
+    }
 }
