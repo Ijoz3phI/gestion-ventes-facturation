3d7448344f54ba2c084c0645bf8c1c4aeea2c061
ljoz3phl <youssefbsibiss2005@gmail.com>
2025-12-15 19:53:06 +0100

    suppression du message

--- src/com/gestionventes/app/Main.java
+++ src/com/gestionventes/app/Main.java
@@ -4,7 +4,6 @@ import com.gestionventes.controller.MenuPrincipal;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour dans l application de gestion de vente");
         new MenuPrincipal().demarrer();
     }
 }
