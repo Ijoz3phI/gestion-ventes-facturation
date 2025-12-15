46ea411c19dc68e382f463fd6a6790f088104dc9
Anass <anassbellagrid@gmail.com>
2025-12-15 16:18:17 +0100

    ProduitDAO

--- src/com/gestionventes/dao/ProduitDAO.java
+++ src/com/gestionventes/dao/ProduitDAO.java
@@ -2,4 +2,17 @@ package com.gestionventes.dao;
 
 public class ProduitDAO {
 
+     private final ArrayList<Produit> produits = new ArrayList<>();
+
+    public void save(Produit produit) { produits.add(produit); }
+
+    public ArrayList<Produit> findAll() { return produits; }
+
+    public Produit findById(int id) {
+        for (Produit p : produits) {
+            if (p.getId() == id) return p;
+        }
+        return null;}
+        
+    public void delete(Produit produit) { produits.remove(produit); }
 }
