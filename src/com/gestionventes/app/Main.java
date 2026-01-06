36b315460b82b38bbbfa6d8cd842111498cfc828
ljoz3phl <youssefbsibiss2005@gmail.com>
2026-01-06 01:24:38 +0100

    backup

--- src/com/gestionventes/app/Main.java
+++ src/com/gestionventes/app/Main.java
@@ -1,14 +1,9 @@
 package com.gestionventes.app;
 
-import com.gestionventes.dao.ClientDAO;
-import com.gestionventes.service.ClientService;
-import com.gestionventes.gui.ClientGUI;
-
-import javax.swing.*;
+import com.gestionventes.controller.MenuPrincipal;
 
 public class Main {
     public static void main(String[] args) {
-        ClientService service = new ClientService(new ClientDAO());
-        SwingUtilities.invokeLater(() -> new ClientGUI(service).setVisible(true));
+        new MenuPrincipal().demarrer();
     }
 }
